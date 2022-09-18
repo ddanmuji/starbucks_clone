2cb4c2da404b1cc42c10501f7e386a072cae62c2
여찬규 <cgyeo5533@gmail.com>
2022-09-18 17:12:02 +0900

    feat: parcel 업데이트 및 적용

--- scripts/clean-cache.sh
+++ scripts/clean-cache.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find . -name ".cache" -print -type d -depth -exec rm -rf {} \;
+find . -name ".parcel-cache" -print -type d -depth -exec rm -rf {} \;
 echo -e "\033[32m" ----- clean dist end ----- "\033[m"
